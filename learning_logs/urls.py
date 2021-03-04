2ffbea92bb388f98d5b50e8e8432c3fb10425dcc
Stuffcode <stuffingeu56@gmail.com>
2021-03-04 18:08:41 +0300

    urls add

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -8,4 +8,8 @@ app_name = 'learning_logs'
 urlpatterns = [
     # Home page
     path('', views.index, name='index'),
+    # Page with list of all topics.
+    path('topics/', views.topics, name='topics'),
+    # Page with information about additional topic.
+    path('topics/<int:topic_id>', views.topic, name='topic')
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
-
-# Create your views here.
+from .models import Topic
 
 def index(request):
     """Home page application Learning lof"""
     return render(request, 'learning_logs/index.html')
     
+def topics(request):
+    """Displays list all topics."""
+    topics = Topic.objects.order_by("date_added")
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
+
+def topic(request, topic_id):
+    """Displays one theame and all its entries."""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by("-date_added")
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
