c412ac64458d603871e17b32c362008ef44f7999
Stuffcode <stuffingeu56@gmail.com>
2021-03-04 08:40:34 +0300

    add urls

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -8,3 +8,19 @@ class Topic(models.Model):
     def __str__(self):
         """Returns str type model."""
         return self.text
+
+class Entry(models.Model):
+    """Information studing by the user on the topic."""
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        """Output string representation model."""
+        if len(self.text) > 50:
+            return f'{self.text[:50]}...'
+        else:
+            return self.text
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    """Home page application Learning lof"""
+    return render(request, 'learning_logs/index.html')
+    
