b422b3a151b69e860ac0e9f6942443915c12f8b0
Andrey Yarovenko <stuffingeu56@gmail.com>
2021-03-09 18:02:35 +0300

    redact file

--- learning_logs/forms.py
+++ learning_logs/forms.py
@@ -2,15 +2,17 @@ from django import forms
 
 from .models import Topic, Entry
 
+
 class TopicForm(forms.ModelForm):
     class Meta:
         model = Topic
         fields = ['text']
-        labels = {'text' : ''}
+        labels = {'text': ''}
+
 
 class EntryForm(forms.ModelForm):
     class Meta:
         model = Entry
         fields = ['text']
-        labels = {'text' : 'Entry'}
+        labels = {'text': 'Entry'}
         widgets = {'text': forms.Textarea(attrs={'cols': 80})}
