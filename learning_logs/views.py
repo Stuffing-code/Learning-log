3667dfe300d52f3bd9df5c3e56f466f436ddc78b
Andrey Yarovenko <stuffingeu56@gmail.com>
2021-03-12 09:04:28 +0300

    add css bootstrap for page

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -11,6 +11,7 @@ def index(request):
     return render(request, 'learning_logs/index.html')
 
 
+@login_required
 def topics(request):
     """Displays list all topics."""
     topics = Topic.objects.filter(owner=request.user).order_by("date_added")
